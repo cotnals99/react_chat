49532d0df8a610afa97a1ae48a0b8f615a1f474e
cotnals99 <su.chae@icloud.com>
2022-09-15 11:14:06 +0700

    SignUp & LogIn Design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Add from '../img/addAvatar.png'
+import Logo from '../img/logo.png'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <img src={Logo} alt="logo" width="50x" />
+            <span className='logo'>DoubleDa Chat</span>
+            <span className='title'>Login</span>
+            <form>
+                <input type="email" placeholder='Enter email' />
+                <input type="password" placeholder='Password' />
+             
+                <button>Sign In</button>
+            </form>
+            <p>You do NOT have an account? Register</p>
+
+        </div>
+    </div>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Add from '../img/addAvatar.png'
+import Logo from '../img/logo.png'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <img src={Logo} alt="logo" width="50x" />
+            <span className='logo'>DoubleDa Chat</span>
+            <span className='title'>Register</span>
+            <form>
+                <input type="text" placeholder='Display Name' />
+                <input type="email" placeholder='Enter email' />
+                <input type="password" placeholder='Password' />
+                <input style={{display:"none"}} type="file" id="file"/>
+                <label htmlFor="file">
+                    <img src={Add} alt="" />
+                    Add an Avatar
+                </label>
+                <button>Sign Up</button>
+            </form>
+            <p>You do have an account? Login</p>
+
+        </div>
+    </div>
   )
 }
 
