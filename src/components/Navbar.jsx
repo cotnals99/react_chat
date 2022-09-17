168c8d0ebe998ee15b91a9565b6863d031777b21
cotnals99 <su.chae@icloud.com>
2022-09-17 11:15:59 +0700

    complete register, login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import {signOut} from 'firebase/auth'
 import {auth} from '../firebase'
+import {AuthContext} from '../context/AuthContext'
 
 const Navbar = () => {
+
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
       <span className='logo'>Doubleda Chat</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/9407178/pexels-photo-9407178.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-        <span>User</span>
+        {/* <img src="https://images.pexels.com/photos/9407178/pexels-photo-9407178.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" /> */}
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
       </div>
       <button onClick={()=>signOut(auth)}>Logout</button>
     </div>
